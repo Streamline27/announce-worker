0ab311904c9f6f5cde63d448e0d2470aa38a770c
Vladislav Yakushin <vladislavs.jakusins@citadele.lv>
2019-05-25 13:19:55 +0300

    first commit

---
+++ src/main/kotlin/lv/announce/worker/WorkerApplication.kt
@@ -0,0 +1,11 @@
+package lv.announce.worker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkerApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorkerApplication>(*args)
+}
